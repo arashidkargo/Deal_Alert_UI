384feca3d5117de4cfed147b4507d2992a4fa617
arashidkargo <AbdullahRashid@KRG-71f9-IFairooz.local>
2018-02-07 15:19:00 -0500

    New Constant Values
    add new constant values URL, add 3 different Url dev,stg,prod. easier
    to point what environment is being used.

--- deal_alert_spec.js
+++ deal_alert_spec.js
@@ -1,7 +1,9 @@
 
  const usrName='kargoqa@gmail.com'
  const usrPassoword='K@rgo123!'
- const url='http://deal-alerts.staging.kargo.com/alerts'
+ const dev='http://deal-alerts.dev.kargo.com/alerts'
+ const stg='http://deal-alerts.dev.kargo.com/alerts'
+ const prod='http://deal-alerts.dev.kargo.com/alerts'
  const auth= 'auth_key'
  const auth_value='Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJjcmVhdGl2ZS1zbmlwcGV0LnByb2Qua2FyZ28uY29tIiwiZXhwIjozMDU5ODU2MDAwMDAwLCJpc3N1ZWQiOjE0ODMxMTY5NTE4NzQsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJjaWQiOiJzZXJ2aWNlIiwiYXBwTmFtZSI6ImNyZWF0aXZlLXNuaXBwZXQifQ.Zg-VXZQfoKbd98uFEuzNE13lDKzIsGj-d2YLhJ1wVVzO_samxBq4pIjWhSy6jCaDJ1D1oCzyKyJy2SErfSvKmhS5qr6jpwxYmiq3nPDpYB0v8fIAD6-6YFNZ0zUX165-Q1cJEXY8dvzx--p1NBVQQyWRQ_yCLHeSw3T9-71LTIxxmR22zllwOmLIml_K01S9P05y48V85r9k9Y4kBESfv86rmv6gI5zc9GljfZ5FuveMdzCPZhmCiz3WUwCUUaBxQ75FU8hk8QzwDeOzRv3tzWco06w9-xL9Xezg20D5onfOFgwafeyujGPdpVIFWAmIFXZQytRIJO1xl2YhUv43bQ'
 
@@ -11,7 +13,7 @@ describe('Deal Alert UI Integration Test', function()
 {
   it('Navigating to Deal Alert Page', function() 
   {
-    cy.visit(url)
+    cy.visit(dev)
   })
 
 
@@ -44,6 +46,7 @@ describe('Deal Alert UI Integration Test', function()
  it('Deal Alert Dashboard', function()
    {
    	cy.contains('Deal Alerts Dashboard')
+  
 	cy.setCookie(auth,auth_value) 
    })
 
@@ -62,7 +65,6 @@ cy.request(url).then((response) => {
 
 
      cy.setCookie(auth,auth_value)
- 
 
 
 })
@@ -75,10 +77,18 @@ it('Click on a Alert', function()
   
   
 
+cy.setCookie(auth,auth_value)
 
  
  
   cy.get(':nth-child(2) > a > .table-link > span').click()    
+
+ 
+    
+
+
+
+
     //cy.setCookie(auth,auth_value)     
   
   ///cy.screenshot(Deal_alert_UI)
@@ -87,11 +97,17 @@ it('Click on a Alert', function()
 
 
 
-  ///cy.get('.breadcrumb--icon-only').click()
+  cy.get('.breadcrumb--icon-only').click()
   
 
 
 })
+
+
+
+
+
+
 
 })
 
